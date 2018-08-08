c03caea30339e818471e9b4904623fb95434959f
jianyinglang <jianying.lang@intel.com>
2018-08-07 17:27:41 -0700

    unitest

---
+++ test/python/test_allreduce.py
@@ -0,0 +1,48 @@
+# ==============================================================================
+#  Copyright 2018 Intel Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ==============================================================================
+"""nGraph TensorFlow bridge cast operation test
+
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import pytest
+
+import tensorflow as tf
+import horovod.tensorflow as hvd
+from common import NgraphTest
+import numpy as np
+
+
+class TestAllreduceOp(NgraphTest):
+  def test_allreduce(self):
+    val = tf.placeholder(tf.float32, shape=(1,))
+
+    with self.device:
+      out = hvd.allreduce(val)
+
+      with self.session as sess:
+        result = sess.run((out,), feed_dict={val: (5.5,)})
+   
+    with tf.device('/cpu:0'):
+      out_cpu = hvd.allreduce(val)
+
+      with self.session as sess:
+        expected = sess.run((out_cpu,), feed_dict={val: (5.5,)})
+    
+    np.testing.assert_allclose(result, expected, rtol=5e-7)
+
