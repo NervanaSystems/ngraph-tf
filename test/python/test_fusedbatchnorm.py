f8dcf9aac5234f580f1445f662eda5a48a16b5a3
Igor Kaplounenko <igor.kaplounenko@intel.com>
2018-08-16 15:26:28 -0700

    works, but rtol looks waaaay high

---
+++ test/python/test_fusedbatchnorm.py
@@ -0,0 +1,55 @@
+# ==============================================================================
+#  Copyright 2018 Intel Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ==============================================================================
+"""nGraph TensorFlow FusedBatchNorm test
+
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+from common import NgraphTest
+
+
+class TestFusedBatchNorm(NgraphTest):
+  def test_fusedbatchnorm(self):
+    x = np.random.rand(64, 3, 10, 8).astype('f')
+    scale = [1.0, 0.9, 1.1]
+    offset = [0.1, 0.2, -.3]
+
+    with self.device:
+      norm = tf.nn.fused_batch_norm(x, scale, offset, data_format='NCHW')
+
+      with self.session as sess:
+        import pdb
+        pdb.set_trace()
+        result = sess.run(norm)
+
+    with tf.device('/cpu:0'):
+      x_t = tf.transpose(x, (0, 2, 3, 1))
+      # tensorflow CPU doesn't support NCHW
+      norm = tf.nn.fused_batch_norm(x_t, scale, offset, data_format='NHWC')
+
+      with self.session as sess:
+        expected = sess.run(norm)
+
+    np.testing.assert_allclose(result[0],
+                               np.transpose(expected[0], (0, 3, 1, 2)),
+                               rtol=0.01)
+    np.testing.assert_allclose(result[1], expected[1], rtol=0.01)
+    np.testing.assert_allclose(result[2], expected[2], rtol=0.01)
