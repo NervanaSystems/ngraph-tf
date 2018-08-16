09af7ff0466199d9aebd8fffa7ba4c3b59942d56
Igor Kaplounenko <igor.kaplounenko@intel.com>
2018-08-16 15:32:13 -0700

    added nhwc test and removed stray pdb

--- test/python/test_fusedbatchnorm.py
+++ test/python/test_fusedbatchnorm.py
@@ -27,23 +27,23 @@ from common import NgraphTest
 
 
 class TestFusedBatchNorm(NgraphTest):
-  def test_fusedbatchnorm(self):
-    x = np.random.rand(64, 3, 10, 8).astype('f')
-    scale = [1.0, 0.9, 1.1]
-    offset = [0.1, 0.2, -.3]
+  x = np.random.rand(64, 3, 10, 8).astype('f')
+  scale = [1.0, 0.9, 1.1]
+  offset = [0.1, 0.2, -.3]
 
+  def test_fusedbatchnorm_nchw(self):
     with self.device:
-      norm = tf.nn.fused_batch_norm(x, scale, offset, data_format='NCHW')
+      norm = tf.nn.fused_batch_norm(self.x, self.scale, self.offset,
+                                    data_format='NCHW')
 
       with self.session as sess:
-        import pdb
-        pdb.set_trace()
         result = sess.run(norm)
 
     with tf.device('/cpu:0'):
-      x_t = tf.transpose(x, (0, 2, 3, 1))
+      x_t = tf.transpose(self.x, (0, 2, 3, 1))
       # tensorflow CPU doesn't support NCHW
-      norm = tf.nn.fused_batch_norm(x_t, scale, offset, data_format='NHWC')
+      norm = tf.nn.fused_batch_norm(x_t, self.scale, self.offset,
+                                    data_format='NHWC')
 
       with self.session as sess:
         expected = sess.run(norm)
@@ -51,5 +51,26 @@ class TestFusedBatchNorm(NgraphTest):
     np.testing.assert_allclose(result[0],
                                np.transpose(expected[0], (0, 3, 1, 2)),
                                rtol=0.01)
+    np.testing.assert_allclose(result[1], expected[1], rtol=0.01)
+    np.testing.assert_allclose(result[2], expected[2], rtol=0.01)
+
+  def test_fusedbatchnorm_nhwc(self):
+    x_t = tf.transpose(self.x, (0, 2, 3, 1))
+
+    with self.device:
+      norm = tf.nn.fused_batch_norm(x_t, self.scale, self.offset,
+                                    data_format='NHWC')
+
+      with self.session as sess:
+        result = sess.run(norm)
+
+    with tf.device('/cpu:0'):
+      norm = tf.nn.fused_batch_norm(x_t, self.scale, self.offset,
+                                    data_format='NHWC')
+
+      with self.session as sess:
+        expected = sess.run(norm)
+
+    np.testing.assert_allclose(result[0], expected[0], rtol=0.01)
     np.testing.assert_allclose(result[1], expected[1], rtol=0.01)
     np.testing.assert_allclose(result[2], expected[2], rtol=0.01)
