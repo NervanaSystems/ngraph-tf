de7f1527ec58520c1c0b441dd0e5823a1f095210
Sayantan Sarkar <sayantan.sarkar@intel.com>
2018-09-06 21:34:22 -0700

    Revert "Adding autopep8 style"
    This reverts commit 479dba087598317890f1df1a93101c03392e71c1.

--- diagnostics/get_node_encapsulate_map.py
+++ diagnostics/get_node_encapsulate_map.py
@@ -1,22 +1,13 @@
 from __future__ import print_function
 
-import pickle as pkl
-import os
-import sys
-import re
-import pdb
+import pickle as pkl, os, sys, re, pdb
 from protobuf_visualize import load_file
 
-
 def create_node_encapsulate_map_pkl(input_dir, output_pkl_name):
-    start_with = "declustered_"
-    ends_with = ".pbtxt"
-    pattern = re.compile("^" + start_with + "(.*?)" + ends_with + "$")
-    # Note: relying on this particular pattern. Could be brittle if the
-    # filenames change
-    declustered_pbtxts = filter(
-        lambda file_name: pattern.search(file_name),
-        os.listdir(input_dir))
+    start_with = "declustered_"; ends_with = ".pbtxt"
+    pattern = re.compile("^"+start_with+"(.*?)"+ends_with+"$")
+    # Note: relying on this particular pattern. Could be brittle if the filenames change
+    declustered_pbtxts = filter(lambda file_name : pattern.search(file_name), os.listdir(input_dir))
     node_cluster_map = {}
     for filename in declustered_pbtxts:
         full_name = os.path.join(input_dir, filename)
@@ -25,13 +16,9 @@ def create_node_encapsulate_map_pkl(input_dir, output_pkl_name):
         print('Processing: ' + filename)
         for idx, node in enumerate(gdef.node):
             if '_ngraph_cluster' in node.attr:
-                node_cluster_map[node.name] = 'encapsulate_' + \
-                    str(node.attr['_ngraph_cluster'].i) + '/'
+                node_cluster_map[node.name] = 'encapsulate_'+str(node.attr['_ngraph_cluster'].i)+'/'
     pkl.dump(node_cluster_map, open(output_pkl_name, "wb"), protocol=2)
-
 
 if __name__ == '__main__':
     create_node_encapsulate_map_pkl(sys.argv[1], sys.argv[2])
-    '''
-    python get_node_encapsulate_map.py /localdisk/sarkars/workspace1/ngraph-models/tensorflow_scripts nodemap.pkl
-    '''
+    # python get_node_encapsulate_map.py /localdisk/sarkars/workspace1/ngraph-models/tensorflow_scripts nodemap.pkl
--- diagnostics/protobuf_visualize.py
+++ diagnostics/protobuf_visualize.py
@@ -1,19 +1,16 @@
 from __future__ import print_function
 
 import ngraph
-import tensorflow as tf
-import numpy as np
-import re
-import os
-import pdb
+import tensorflow as tf, numpy as np, re, os, pdb
 from google.protobuf import text_format
 from tensorflow.core.framework import graph_pb2
 from tensorflow.python.platform import gfile
-import argparse
-import pickle as pkl
-
+import argparse, pickle as pkl
 
 def modify_node_names(graph_def, node_map):
+    #aaa = [ii for ii in graph_def.node if 'Reshape' == ii.name]
+    #bbb = [ii for ii in graph_def.node if 'Decode/get_center_coordinates_and_sizes/transpose/sub_1/_490__cf__496' == ii.name]
+    #pdb.set_trace()
     for node in graph_def.node:
         if node.name in node_map:
             old_name = node.name
@@ -27,40 +24,29 @@ def modify_node_names(graph_def, node_map):
                     colon_split = inp_name.split(':')
                     assert len(colon_split) <= 2
                     control_dependency_part = '^' if inp_name[0] == '^' else ''
-                    colon_part = '' if len(
-                        colon_split) == 1 else ':' + colon_split[1]
+                    colon_part = '' if len(colon_split) == 1 else ':'+colon_split[1]
                     if inp_name.lstrip('^').split(':')[0] == old_name:
-                        _node.input[idx] = control_dependency_part + \
-                            new_name + colon_part
-            # TODO: Do we need to edit this anywhere else other than inputs?
+                        _node.input[idx] = control_dependency_part + new_name + colon_part
+                #TODO: Do we need to edit this anywhere else other than inputs?
+
+    
+    #aaa1 = [ii for ii in graph_def.node if node_map['Reshape'] == ii.name]
+    #bbb1 = [ii for ii in graph_def.node if node_map['Decode/get_center_coordinates_and_sizes/transpose/sub_1/_490__cf__496'] == ii.name]
+    #pdb.set_trace()
     return graph_def
 
-
+# remove '_' from node names
 def sanitize_node_names(graph_def):
-    '''
-    remove '_' from node names
-    '''
-    return modify_node_names(graph_def, {
-                             node.name: node.name[1:] for node in graph_def.node if node.name[0] == "_"})
+    return modify_node_names(graph_def, {node.name : node.name[1:] for node in graph_def.node if node.name[0] == "_"})
 
-
+# prepend an extra string to the node name (presumably a scope, to denote encapsulate)
 def prepend_to_name(graph_def, node_map):
-    '''
-    prepend an extra string to the node name (presumably a scope, to denote encapsulate)
-    '''
-    return modify_node_names(graph_def, {
-                             node.name: node_map[node.name] + node.name for node in graph_def.node if node.name in node_map})
+    return modify_node_names(graph_def, {node.name : node_map[node.name] + node.name for node in graph_def.node if node.name in node_map})
 
-
+# can load protobuf (pb or pbtxt). can modify only pbtxt for now
 def load_file(graph_file, input_binary, modifier_function_list=[]):
-    '''
-    can load and modify protobuf (pb or pbtxt)
-    '''
     if not gfile.Exists(graph_file):
-        raise Exception(
-            "Input graph file '" +
-            graph_file +
-            "' does not exist!")
+        raise Exception("Input graph file '" + graph_file + "' does not exist!")
 
     graphdef = graph_pb2.GraphDef()
     with open(graph_file, "r") as f:
@@ -70,30 +56,20 @@ def load_file(graph_file, input_binary, modifier_function_list=[]):
                 graphdef.ParseFromString(protobuf_str)
             else:
                 text_format.Merge(protobuf_str, graphdef)
-        except BaseException:
-            raise Exception(
-                "Failed to read pb or pbtxt. input_binary is " +
-                str(input_binary) +
-                " maybe try flipping it?")
+        except:
+            raise Exception("Failed to read pb or pbtxt. input_binary is " + str(input_binary) + " maybe try flipping it?")
     for modifier_function in modifier_function_list:
         graphdef = modifier_function(graphdef)
     return graphdef
 
-
 def preprocess(input_filename, out_dir, input_binary, node_map):
     # Note: node_map should be applied before sanitize_node_names.
-    # Else sanitize_node_names might change the node names, which might become
-    # unrecognizable to node_map
-    modifiers = [
-        lambda pbtxt_str: prepend_to_name(
-            pbtxt_str,
-            node_map),
-        sanitize_node_names]
+    # Else sanitize_node_names might change the node names, which might become unrecognizable to node_map
+    modifiers = [lambda pbtxt_str : prepend_to_name(pbtxt_str, node_map), sanitize_node_names]
     gdef = load_file(input_filename, input_binary, modifiers)
-    if not os.path.exists(out_dir):  # create output dir if it does not exist
+    if not os.path.exists(out_dir): #create output dir if it does not exist
         os.makedirs(out_dir)
     return gdef
-
 
 def graphdef_to_dot(gdef, dot_output):
     with open(dot_output, "wb") as f:
@@ -106,52 +82,34 @@ def graphdef_to_dot(gdef, dot_output):
                 input_name = re.sub(r"^\^", "", parts[0])
                 print("  \"" + input_name + "\" -> \"" + output_name + "\";", file=f)
         print("}", file=f)
-    print("\n" + ('=-' * 30))
+    print("\n"+('=-'*30))
     print("Created DOT file '" + dot_output + "'.")
-    print("Can be converted to pdf using: dot -Tpdf " +
-          dot_output + " -o " + dot_output + ".pdf")
-    print('=-' * 30)
-
+    print("Can be converted to pdf using: dot -Tpdf "+dot_output+" -o "+dot_output+".pdf")
+    print('=-'*30)
 
 def protobuf_to_dot(input_filename, dot_dir, input_binary=False, node_map={}):
     gdef = preprocess(input_filename, dot_dir, input_binary, node_map)
-    graphdef_to_dot(
-        gdef,
-        dot_dir.rstrip('/') +
-        '/' +
-        os.path.basename(input_filename) +
-        '.dot')
-
+    graphdef_to_dot(gdef, dot_dir.rstrip('/') + '/' + os.path.basename(input_filename) + '.dot')
 
 def graphdef_to_tensorboard(gdef, tensorboard_output):
     # convert graphdef to graph, even though FileWriter can accepts graphdefs.
-    # this is because FileWriter has deprecated graphdef as inputs, and
-    # prefers graphs as inputs
+    # this is because FileWriter has deprecated graphdef as inputs, and prefers graphs as inputs
     with tf.Session() as sess:
         tf.import_graph_def(gdef)
         writer = tf.summary.FileWriter(tensorboard_output, sess.graph)
-        # TODO: try with tf master
+        #TODO: try with tf master
         # wont work now if we have NGraphVariable, NGraphEncapsulateOp
-        # TODO: How about supporting NGraphVariable and NGraphEncapsulateOp by
-        # switching their optype with something TB knows
+        # TODO: How about supporting NGraphVariable and NGraphEncapsulateOp by switching their optype with something TB knows
         writer.flush()
         writer.close()
-    # It seems NGraphVariable and NGraphEncapsulateOp are registered in C++
-    # but not in python
-    print("\n" + ('=-' * 30) + "\nTo view Tensorboard:")
+    # It seems NGraphVariable and NGraphEncapsulateOp are registered in C++ but not in python
+    print("\n"+('=-'*30)+"\nTo view Tensorboard:")
     print("1) Run this command: tensorboard --logdir " + tensorboard_output)
-    print("2) Go to the URL it provides or http://localhost:6006/\n" +
-          ('=-' * 30) + "\n")
+    print("2) Go to the URL it provides or http://localhost:6006/\n"+('=-'*30)+"\n")
 
-
-def protobuf_to_grouped_tensorboard(
-        input_filename,
-        tensorboard_dir,
-        input_binary=False,
-        node_map={}):
+def protobuf_to_grouped_tensorboard(input_filename, tensorboard_dir, input_binary=False, node_map={}):
     gdef = preprocess(input_filename, tensorboard_dir, input_binary, node_map)
     graphdef_to_tensorboard(gdef, tensorboard_dir)
-
 
 visualizations_supported = [protobuf_to_dot, protobuf_to_grouped_tensorboard]
 
@@ -167,29 +125,13 @@ if __name__ == "__main__":
     python protobuf_visualize.py -c nodemap.pkl pbtxtfile.pbtxt ./vis  # read pbtxt, remap node names and generate TB
     One can also import the file and use its functions
     '''
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawTextHelpFormatter,
-        description=helptxt)
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description=helptxt)
     parser.add_argument("input", help="The input protobuf (pb or pbtxt)")
     parser.add_argument("out", help="The output directory")
-    parser.add_argument(
-        '-b',
-        dest='binary',
-        action='store_true',
-        help="Add this flag to indicate its a .pb. Else it is assumed to be a .pbtxt")
-    parser.add_argument(
-        "-v",
-        "--visualize",
-        type=int,
-        default=1,
-        help="Enter 0 (protobuf->dot) or 1 (protobuf->Tensorboard). By default it converts to tensorboard")
-    parser.add_argument(
-        "-c",
-        "--cluster",
-        help="An file that contains the node-to-cluster map that can be used to group them into clusters")
+    parser.add_argument('-b', dest='binary', action='store_true', help="Add this flag to indicate its a .pb. Else it is assumed to be a .pbtxt")
+    parser.add_argument("-v", "--visualize", type=int, default=1, help="Enter 0 (protobuf->dot) or 1 (protobuf->Tensorboard). By default it converts to tensorboard")
+    parser.add_argument("-c", "--cluster", help="An file that contains the node-to-cluster map that can be used to group them into clusters")
     args = parser.parse_args()
 
-    node_map = {} if args.cluster is None else pkl.load(
-        open(args.cluster, 'rb'))
-    visualizations_supported[args.visualize](
-        args.input, args.out, args.binary, node_map)
+    node_map = {} if args.cluster is None else pkl.load(open(args.cluster, 'rb'))
+    visualizations_supported[args.visualize](args.input, args.out, args.binary, node_map)
