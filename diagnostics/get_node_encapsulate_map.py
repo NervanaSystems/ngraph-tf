53a60125210d35aaa29bd935989cbfa68a646f94
Sayantan Sarkar <sayantan.sarkar@intel.com>
2018-09-07 10:14:48 -0700

    Incorporating comments

--- diagnostics/get_node_encapsulate_map.py
+++ diagnostics/get_node_encapsulate_map.py
@@ -1,3 +1,19 @@
+# ==============================================================================
+#  Copyright 2018 Intel Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ==============================================================================
+
 from __future__ import print_function
 
 import pickle as pkl
@@ -30,4 +46,4 @@ def create_node_encapsulate_map_pkl(input_dir, output_pkl_name):
 
 if __name__ == '__main__':
     create_node_encapsulate_map_pkl(sys.argv[1], sys.argv[2])
-    # python get_node_encapsulate_map.py /localdisk/sarkars/workspace1/ngraph-models/tensorflow_scripts nodemap.pkl
+    # python get_node_encapsulate_map.py /path/to/where/the/declustered/pbtxts/were/dumped nodemap.pkl
