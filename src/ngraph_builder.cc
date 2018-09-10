1647acdab20fb0d5da49dbffda467c78a8e64236
jianyinglang <jianying.lang@intel.com>
2018-09-10 16:29:50 -0700

    Remove the rejection mark for is_training=false case in FusedBatchNormGrad to avoid the segfault in resnet50 training run.

--- src/ngraph_builder.cc
+++ src/ngraph_builder.cc
@@ -1422,6 +1422,8 @@ static Status TranslateFusedBatchNormGradOp(
   TF_RETURN_IF_ERROR(ValidateInputCount(op, 5));
 
   bool tf_is_training;
+  // Now we only support is_training=true case. We will either mark
+  // or implement is_training=false case later.
   if (GetNodeAttr(op->attrs(), "is_training", &tf_is_training) !=
       Status::OK()) {
     NGRAPH_VLOG(3) << "is_training attribute not present, setting to true";
