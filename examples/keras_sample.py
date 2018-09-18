e4de63e54e70aadd5bdedaedcbb9c1bfff9be7f1
Sayantan Sarkar <sayantan.sarkar@intel.com>
2018-09-18 15:43:48 -0700

    Adding training in keras example

--- examples/keras_sample.py
+++ examples/keras_sample.py
@@ -13,12 +13,20 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # ==============================================================================
+import tensorflow as tf
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 import numpy as np
 import ngraph
 
+# A simple script to run inference and training on resnet 50
+
 model = ResNet50(weights='imagenet')
 
-img = np.random.rand(1, 224, 224, 3)
+batch_size = 128
+img = np.random.rand(batch_size, 224, 224, 3)
 preds = model.predict(preprocess_input(img))
 print('Predicted:', decode_predictions(preds, top=3)[0])
+model.compile(tf.keras.optimizers.SGD(), loss='categorical_crossentropy')
+preds = model.fit(
+    preprocess_input(img), np.zeros((batch_size, 1000), dtype='float32'))
+print('Ran a train round')
