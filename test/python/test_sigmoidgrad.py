4222fddf5225e2535e0ac94c854fd71f00b02ef3
mingshan.wang <mingshan@nervana-skx98.fm.intel.com>
2018-09-25 16:42:33 -0700

    More test

--- test/python/test_sigmoidgrad.py
+++ test/python/test_sigmoidgrad.py
@@ -30,7 +30,6 @@ from common import NgraphTest
 
 import numpy as np
 
-
 class TestSigmoidGradOperations(NgraphTest):
 
     def test_sigmoidgrad_2d(self):
@@ -43,9 +42,6 @@ class TestSigmoidGradOperations(NgraphTest):
 
         def run_test(sess):
             return sess.run(out)
-
-        print("ngraph ", self.with_ngraph(run_test))
-        print("cpu ", self.without_ngraph(run_test))
 
         assert np.allclose(
             self.with_ngraph(run_test), self.without_ngraph(run_test))
@@ -62,96 +58,19 @@ class TestSigmoidGradOperations(NgraphTest):
         def run_test(sess):
             return sess.run(out)
 
-        print("ngraph ", self.with_ngraph(run_test))
-        print("cpu ", self.without_ngraph(run_test))
-
         assert np.allclose(
             self.with_ngraph(run_test), self.without_ngraph(run_test))
 
-    #@pytest.mark.parametrize("padding", ("VALID", "SAME"))
-    # def test_nchw(self, padding):
-    #     # The expected size of the backprop will depend on whether padding is VALID
-    #     # or SAME.
-    #     out_backprop_in_sizes = self.OUT_BACKPROP_IN_SIZES[padding]
-    #     x1, x2 = self.make_filter_and_backprop_args(out_backprop_in_sizes)
+    def test_sigmoidgrad_5d(self):
+        y = constant_op.constant(
+            self.generate_random_numbers(240240, 1.0, 15.0), shape=[10,11,12,13,14])
+        y_delta = constant_op.constant(
+            self.generate_random_numbers(240240, -3.0, 10.0), shape=[10,11,12,13,14])
 
-    #     def run_test_ngraph(sess):
-    #         t1 = constant_op.constant(self.INPUT_SIZES_NCHW)
-    #         t2 = constant_op.constant(x2, shape=self.FILTER_IN_SIZES)
-    #         t3 = constant_op.constant(x1, shape=out_backprop_in_sizes)
-    #         inp = nn_ops.conv2d_backprop_input(
-    #             t1,
-    #             t2,
-    #             t3,
-    #             strides=[1, 1, 2, 2],
-    #             padding=padding,
-    #             data_format='NCHW')
-    #         return sess.run(inp)
+        out = sigmoid_grad(y, y_delta)
 
-    #     # To validate on the CPU side we will need to run in NHWC, because the CPU
-    #     # implementation of conv/conv backprop does not support NCHW. We will
-    #     # transpose on the way in and on the way out.
-    #     def run_test_tf(sess):
-    #         t1 = constant_op.constant(self.INPUT_SIZES_NHWC)
-    #         t2 = constant_op.constant(x2, shape=self.FILTER_IN_SIZES)
-    #         t3 = constant_op.constant(x1, shape=out_backprop_in_sizes)
-    #         t3 = tf.transpose(t3, [0, 2, 3, 1])
-    #         inp = nn_ops.conv2d_backprop_input(
-    #             t1,
-    #             t2,
-    #             t3,
-    #             strides=[1, 2, 2, 1],
-    #             padding=padding,
-    #             data_format='NHWC')
-    #         inp = tf.transpose(inp, [0, 3, 1, 2])
-    #         return sess.run(inp)
-
-    #     assert np.allclose(
-    #         self.with_ngraph(run_test_ngraph), self.without_ngraph(run_test_tf))
-
-    # def test_sigmoidgrad_2d(self):
-    #     #gradients = constant_op.constant(
-    #     #    self.generate_random_numbers(6, 1.0, 10.0), shape=[2, 3])
-    #     y = constant_op.constant(
-    #         self.generate_random_numbers(6, 1.0, 10.0), shape=[2, 3])
-    #     y_delta = constant_op.constant(
-    #         self.generate_random_numbers(6, 0.0, 100.0), shape=[2, 3])
-
-    #     # Run on nGraph
-    #     out = sigmoid_grad(y, y_delta)
-    #     with self.session as sess:
-    #     	result = sess.run(out)
-
-    # Run on CPU
-
-
-#ngraph.disable()
-#if ngraph.is_enabled():
-#	print ("ngraph is enabled not as expected")
-#out = sigmoid_grad(y, y_delta)
-#with self.session as sess:
-#        expected = sess.run(out)
-
-#assert (result == expected).all()
-
-#def test_sigmoidgrad_1d(self):
-#    gradients = constant_op.constant(
-#        self.generate_random_numbers(100, 123.0, 345.0), shape=[100])
-#    features = constant_op.constant(
-#        self.generate_random_numbers(100, 567.0, 789.0), shape=[100])
-
-#    # Run on nGraph
-#    ngraph.disable()
-#    out = sigmoid_grad(gradients, features)
-#    with self.session as sess:
-#    	result = sess.run(out)
-
-#    # Run on CPU
-#    #ngraph.disable()
-#    #if ngraph.is_enabled():
-#    #	print ("ngraph is enabled not as expected")
-#    #out = sigmoid_grad(gradients, features)
-#    #with self.session as sess:
-#    #	expected = sess.run(out)
-
-#    #assert (result == expected).all()
+        def run_test(sess):
+            return sess.run(out)
+            
+        assert np.allclose(
+            self.with_ngraph(run_test), self.without_ngraph(run_test))
