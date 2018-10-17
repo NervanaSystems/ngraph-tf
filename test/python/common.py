87ef6130b5583d6f750073311e55b9eb2b43092e
Sayantan Sarkar <sayantan.sarkar@intel.com>
2018-10-16 21:31:46 -0700

    Enable one python test

--- test/python/common.py
+++ test/python/common.py
@@ -3,6 +3,8 @@ import platform
 import random
 
 import tensorflow as tf
+from tensorflow.core.protobuf import config_pb2
+from tensorflow.core.protobuf import rewriter_config_pb2
 
 __all__ = ['LIBNGRAPH_BRIDGE', 'NgraphTest']
 
@@ -10,14 +12,23 @@ _ext = 'dylib' if platform.system() == 'Darwin' else 'so'
 
 LIBNGRAPH_BRIDGE = 'libngraph_bridge.' + _ext
 
-
 class NgraphTest(object):
 
-    def with_ngraph(self, l, config=tf.ConfigProto()):
+    def default_config(self):
+        config = tf.ConfigProto() #config_pb2.ConfigProto()
+        config.allow_soft_placement = True
+        config.graph_options.optimizer_options.opt_level = -1
+        config.graph_options.rewrite_options.constant_folding = (
+            rewriter_config_pb2.RewriterConfig.OFF)
+
+    def with_ngraph(self, l, config=None):
+        if config is None:
+            config = self.default_config()
         ngraph_tf_disable = os.environ.pop('NGRAPH_TF_DISABLE', None)
         ngraph_tf_disable_deassign_clusters = os.environ.pop(
             'NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS', None)
 
+        os.environ['NGRAPH_TF_LOG_PLACEMENT'] = '1'
         os.environ['NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS'] = '1'
 
         with tf.Session(config=config) as sess:
@@ -33,11 +44,14 @@ class NgraphTest(object):
 
         return retval
 
-    def without_ngraph(self, l, config=tf.ConfigProto()):
+    def without_ngraph(self, l, config=None):
+        if config is None:
+            config = self.default_config()
         ngraph_tf_disable = os.environ.pop('NGRAPH_TF_DISABLE', None)
         ngraph_tf_disable_deassign_clusters = os.environ.pop(
             'NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS', None)
 
+        os.environ['NGRAPH_TF_LOG_PLACEMENT'] = '1'
         os.environ['NGRAPH_TF_DISABLE'] = '1'
 
         with tf.Session(config=config) as sess:
