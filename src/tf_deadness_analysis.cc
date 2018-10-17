9666526b7e5ef7236ed788f79e0eeb741564fec2
shresthamalik <shrestha.malik@intel.com>
2018-10-16 19:06:39 -0700

    Add Intel HEader

--- src/tf_deadness_analysis.cc
+++ src/tf_deadness_analysis.cc
@@ -20,6 +20,22 @@ Revision: 6619dd5fdcad02f087f5758083e2585bdfef9e78
 File: tensorflow/tensorflow/compiler/jit/deadness_analysis.cc
 
 *******************************************************************************/
+/*******************************************************************************
+ * Copyright 2017-2018 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
 #include "ngraph_utils.h"
 #if !defined(NGRAPH_TF_DISABLE_DEADNESS_CHECK)
 
