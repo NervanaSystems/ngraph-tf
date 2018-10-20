3e861e6737ccbb41f09b195b0e60b63a5fd52def
Sayantan Sarkar <sayantan.sarkar@intel.com>
2018-10-20 00:25:24 -0700

    Using import ngraph instead of raw .so load

--- test/python/common.py
+++ test/python/common.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 from tensorflow.core.protobuf import config_pb2
 from tensorflow.core.protobuf import rewriter_config_pb2
 
+import ngraph
+
 __all__ = ['LIBNGRAPH_BRIDGE', 'NgraphTest']
 
 _ext = 'dylib' if platform.system() == 'Darwin' else 'so'
@@ -18,27 +20,24 @@ class NgraphTest(object):
     def default_config(self):
         config = tf.ConfigProto()  #config_pb2.ConfigProto()
         config.allow_soft_placement = True
-        config.graph_options.optimizer_options.opt_level = -1
-        config.graph_options.rewrite_options.constant_folding = (
-            rewriter_config_pb2.RewriterConfig.OFF)
+        #config.graph_options.optimizer_options.opt_level = -1
+        #config.graph_options.rewrite_options.constant_folding = (
+        #    rewriter_config_pb2.RewriterConfig.OFF)
 
     def with_ngraph(self, l, config=None):
         if config is None:
             config = self.default_config()
-        ngraph_tf_disable = os.environ.pop('NGRAPH_TF_DISABLE', None)
         ngraph_tf_disable_deassign_clusters = os.environ.pop(
             'NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS', None)
 
         os.environ['NGRAPH_TF_LOG_PLACEMENT'] = '1'
         os.environ['NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS'] = '1'
-
+        ngraph.enable()
         with tf.Session(config=config) as sess:
             retval = l(sess)
 
         os.environ.pop('NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS', None)
 
-        if ngraph_tf_disable is not None:
-            os.environ['NGRAPH_TF_DISABLE'] = ngraph_tf_disable
         if ngraph_tf_disable_deassign_clusters is not None:
             os.environ['NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS'] = \
                 ngraph_tf_disable_deassign_clusters
@@ -48,20 +47,18 @@ class NgraphTest(object):
     def without_ngraph(self, l, config=None):
         if config is None:
             config = self.default_config()
-        ngraph_tf_disable = os.environ.pop('NGRAPH_TF_DISABLE', None)
         ngraph_tf_disable_deassign_clusters = os.environ.pop(
             'NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS', None)
 
         os.environ['NGRAPH_TF_LOG_PLACEMENT'] = '1'
         os.environ['NGRAPH_TF_DISABLE'] = '1'
 
+        ngraph.disable()
         with tf.Session(config=config) as sess:
             retval = l(sess)
 
         os.environ.pop('NGRAPH_TF_DISABLE', None)
 
-        if ngraph_tf_disable is not None:
-            os.environ['NGRAPH_TF_DISABLE'] = ngraph_tf_disable
         if ngraph_tf_disable_deassign_clusters is not None:
             os.environ['NGRAPH_TF_DISABLE_DEASSIGN_CLUSTERS'] = \
                 ngraph_tf_disable_deassign_clusters
