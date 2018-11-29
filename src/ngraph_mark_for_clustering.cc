a0249c06a7e18b722f2ae506adbf4fdd1ea2c8aa
shresthamalik <shrestha.malik@intel.com>
2018-11-29 15:24:03 -0800

    NGRAPH_TF_BACKEND errors out if the backend is invalid

--- src/ngraph_mark_for_clustering.cc
+++ src/ngraph_mark_for_clustering.cc
@@ -553,7 +553,7 @@ Status MarkForClustering(Graph* graph) {
     if (backend_env.empty() ||
         !BackendManager::IsSupportedBackend(backend_env)) {
       return errors::Internal("NGRAPH_TF_BACKEND: ", backend_env,
-                              "is not supported");
+                              " is not supported");
     }
     current_backend = backend_env;
   }
