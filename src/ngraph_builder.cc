73432551f1ab0b676a6c3b001d0651920bd1a836
Sayantan Sarkar <sayantan.sarkar@intel.com>
2018-11-30 10:19:02 -0800

    Code format

--- src/ngraph_builder.cc
+++ src/ngraph_builder.cc
@@ -3517,8 +3517,10 @@ static Status TranslateStridedSliceOp(
     // The first 2 cases breaks down this range
     if (idx >= 0 && idx <= (dim - 1)) {
       return idx;
-    } else if (idx < 0 && idx + static_cast<int>(dim) >= 0) {  // careful not to do idx >= -dim
-                                             // (since dim is unsigned)
+    } else if (idx < 0 &&
+               idx + static_cast<int>(dim) >=
+                   0) {       // careful not to do idx >= -dim
+                              // (since dim is unsigned)
       return idx + (int)dim;  // Type casting to int to enable unambiguous auto
                               // type inference of return type
     } else if (idx > dim - 1) {
