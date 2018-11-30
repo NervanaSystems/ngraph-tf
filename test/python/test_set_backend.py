feb3623070ae1de72e1b0c7c7a6824694765afff
shresthamalik <shrestha.malik@intel.com>
2018-11-29 17:48:27 -0800

    Incorporate review comments

--- test/python/test_set_backend.py
+++ test/python/test_set_backend.py
@@ -23,13 +23,23 @@ import ngraph_bridge
 # Test ngraph_bridge config options
 def test_set_backend():
     ngraph_bridge.enable()
+    backend_cpu = 'CPU'
+    backend_interpreter = 'INTERPRETER'
+
+    found_cpu = False
+    found_interpreter = False
     # These will only print when running pytest with flag "-s"
     print("Number of supported backends ", ngraph_bridge.backends_len())
     supported_backends = ngraph_bridge.list_backends()
     print(" ****** Supported Backends ****** ")
     for backend_name in supported_backends:
-        print (backend_name)
+        print(backend_name)
+        if backend_name == backend_cpu:
+            found_cpu = True
+        if backend_name == backend_interpreter:
+            found_interpreter = True
     print(" ******************************** ")
+    assert (found_cpu and found_interpreter) == True
 
     # Create Graph
     val = tf.placeholder(tf.float32)
@@ -37,7 +47,6 @@ def test_set_backend():
     out2 = tf.abs(out1)
 
     # set INTERPRETER backend
-    backend_interpreter = 'INTERPRETER'
     assert ngraph_bridge.is_supported_backend(backend_interpreter) == True 
     ngraph_bridge.set_backend(backend_interpreter)
     currently_set_backend = ngraph_bridge.get_currently_set_backend_name()
@@ -52,8 +61,7 @@ def test_set_backend():
     assert currently_set_backend == backend_interpreter
 
     # set CPU backend
-    backend_cpu = 'CPU'
-    assert ngraph_bridge.is_supported_backend(backend_interpreter) == True 
+    assert ngraph_bridge.is_supported_backend(backend_cpu) == True 
     ngraph_bridge.set_backend(backend_cpu)
     currently_set_backend = ngraph_bridge.get_currently_set_backend_name()
     assert currently_set_backend == backend_cpu
