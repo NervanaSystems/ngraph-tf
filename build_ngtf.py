088704bc3dbcf23e3b84125b8b5926e55ce9b638
Avijit <30507445+avijit-nervana@users.noreply.github.com>
2018-12-10 23:06:57 -0800

    Update build_ngtf.py

--- build_ngtf.py
+++ build_ngtf.py
@@ -406,7 +406,7 @@ def main():
     os.chdir(build_dir)
 
     # Component versions
-    ngraph_version = "90aa7336dfbf21d3ecab1b50aa8676c2fe1bd75d"
+    ngraph_version = "v0.11.0-rc.0"
     tf_version = "v1.12.0"
     venv_dir = 'venv-tf-py3'
     artifacts_location = 'artifacts'
