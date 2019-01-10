2e54e0f7b5c913b0bb9b6fea2248793c981a3b34
Avijit <Avijit.Chakraborty@intel.com>
2019-01-10 19:08:00 +0530

    Added header file installations for ngraph-tf

--- examples/tf_cpp_examples/hello_tf.cpp
+++ examples/tf_cpp_examples/hello_tf.cpp
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-#include "ngraph_builder.h"
-#include "ngraph_utils.h"
 
 #include "tensorflow/core/framework/graph.pb.h"
 #include "tensorflow/core/framework/op.h"
