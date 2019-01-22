1026bd42874623ad03ccce2e8cb6ee58a9538f91
jianyinglang <jianying.lang@intel.com>
2019-01-22 15:34:30 -0800

    Memory check function

--- src/ngraph_encapsulate_op.cc
+++ src/ngraph_encapsulate_op.cc
@@ -207,6 +207,26 @@ class NGraphEncapsulateOp : public OpKernel {
     return Status::OK();
   }
 
+  void process_mem_usage(double& vm_usage, double& resident_set) {
+    vm_usage = 0.0;
+    resident_set = 0.0;
+
+    // the two fields we want
+    unsigned long vsize;
+    long rss;
+    {
+      std::string ignore;
+      std::ifstream ifs("/proc/self/stat", std::ios_base::in);
+      ifs >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+      >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+      >> ignore >> ignore >> vsize >> rss;
+    }
+
+    long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+    vm_usage = vsize / 1024.0;
+    resident_set = rss * page_size_kb;
+  }
+
   // TODO(amprocte): this needs to be made thread-safe (compilation cache OK?).
   void Compute(OpKernelContext* ctx) override {
     std::lock_guard<std::mutex> lock(m_compute_lock);
