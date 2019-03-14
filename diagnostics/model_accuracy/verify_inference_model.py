ba94610ae0b748ebde6e2ed79bc8ffdd2753331d
sindhura.kantamani <skantama@nervana-skx99.fm.intel.com>
2019-03-13 17:03:47 -0700

    Addressed avijit's comments

--- diagnostics/model_accuracy/verify_inference_model.py
+++ diagnostics/model_accuracy/verify_inference_model.py
@@ -39,69 +39,71 @@ def download_repo(repo, target_name=None, version='master'):
     command_executor("git clone " + repo)
 
 
-def run_inference(model_name):
-    parameters = '[{"model_type" : "Image Recognition", "model_name" : "Inception_v4","DATASET" : "/mnt/data/TF_ImageNet_latest/","CHECKPOINT" : "/nfs/site/home/skantama/validation/models/research/checkpoints/inception_v4.ckpt"}, {"model_type" : "Image Recognition", "model_name" : "MobileNet_v1","DATASET" : "/mnt/data/TF_ImageNet_latest/","CHECKPOINT" : "/nfs/site/home/skantama/validation/models/research/checkpoints/mobilenet_v1_1.0_224.ckpt"}, {"model_type" : "Image Recognition", "model_name" : "ResNet50_v1","DATASET" : "/mnt/data/TF_ImageNet_latest/","CHECKPOINT" : "/nfs/site/home/skantama/validation/models/research/checkpoints/resnet_v1_50.ckpt"}, {"model_type" : "Object Detection", "model_name" : "SSD-MobileNet_v1", "CHECKPOINT" : "/nfs/site/disks/aipg_trained_dataset/ngraph_tensorflow/fully_trained/ssd_mobilenet_v1_coco_2018_01_28/"}]'
+def run_inference(model_name, models_dir):
+    parameters = \
+    '[{"model_type" : "Image Recognition", "model_name" : "Inception_v4", \
+    "cmd" : "OMP_NUM_THREADS=28 KMP_AFFINITY=granularity=fine,compact,1,0 python eval_image_classifier.py \
+    --alsologtostderr --checkpoint_path=/nfs/site/home/skantama/validation/models/research/checkpoints/inception_v4.ckpt \
+    --dataset_dir=/mnt/data/TF_ImageNet_latest/ --dataset_name=imagenet \
+    --dataset_split_name=validation --model_name=inception_v4"},\
+     {"model_type" : "Image Recognition", "model_name" : "MobileNet_v1", \
+     "cmd" : "OMP_NUM_THREADS=28 KMP_AFFINITY=granularity=fine,compact,1,0 python eval_image_classifier.py \
+     --alsologtostderr --checkpoint_path=/nfs/site/home/skantama/validation/models/research/checkpoints/mobilenet_v1_1.0_224.ckpt \
+     --dataset_dir=/mnt/data/TF_ImageNet_latest/ --dataset_name=imagenet \
+     --dataset_split_name=validation --model_name=mobilenet_v1"}, \
+     {"model_type" : "Image Recognition", "model_name" : "ResNet50_v1", \
+     "cmd" : "OMP_NUM_THREADS=28 KMP_AFFINITY=granularity=fine,compact,1,0 python eval_image_classifier.py \
+     --alsologtostderr --checkpoint_path=/nfs/site/home/skantama/validation/models/research/checkpoints/resnet_v1_50.ckpt \
+     --dataset_dir=/mnt/data/TF_ImageNet_latest/ --dataset_name=imagenet \
+     --dataset_split_name=validation --model_name=resnet_v1_50 --labels_offset=1"}, \
+     {"model_type" : "Object Detection", "model_name" : "SSD-MobileNet_v1", \
+     "cmd" : "OMP_NUM_THREADS=28 KMP_AFFINITY=granularity=fine,compact,1,0 python object_detection/model_main.py --logtostderr \
+     --pipeline_config_path=object_detection/samples/configs/ssd_mobilenet_v1_coco.config \
+     --checkpoint_dir=/nfs/site/disks/aipg_trained_dataset/ngraph_tensorflow/fully_trained/ssd_mobilenet_v1_coco_2018_01_28/ \
+     --run_once=True"}]'
+
     try:
         data = json.loads(parameters)
     except:
         print("Pass a valid model prameters dictionary")
-
-    repo = "https://github.com/tensorflow/models.git"
     pwd = os.getcwd()
 
     for i, d in enumerate(data):
         if (model_name in data[i]["model_name"]):
-            download_repo(repo)
             if (data[i]["model_type"] == "Image Recognition"):
-                CHECKPOINT = data[i]["CHECKPOINT"]
-                DATASET_DIR = data[i]["DATASET"]
-                os.chdir(pwd + "/models/research/slim")
+                if models_dir is None:
+                    os.chdir(pwd + "/models/research/slim")
+                else:
+                    os.chdir(models_dir + "research/slim")
                 command_executor("export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`")
                 command_executor('git apply ' + pwd +
                                  '/image_recognition.patch')
 
-            #TODO:SSD-MobileNet-v1 is not work, will re-enable this after debugging
+            #TODO:SSD-MobileNet-v1 does not run, will re-enable this after debugging
             # if(data[i]["model_type"] == "Object Detection"):
-            #     CHECKPOINT = data[i]["CHECKPOINT"]
             #     command_executor('git apply ' + pwd + '/object_detection.patch')
+            #     if models_dir is None:
+            #       os.chdir(pwd + "/models/research/object_detection")
+            #       else:
+            #       os.chdir(models_dir + "research/object_detection")
             #     os.chdir(pwd + "/models/research/")
             #     command_executor("python setup.py install")
             #     command_executor("python slim/setup.py install")
             #     command_executor("export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/object_detection:`pwd`/slim")
-    try:
-        if (model_name in "Inception_v4"):
-            cmd = "OMP_NUM_THREADS=28 KMP_AFFINITY=granularity=fine,compact,1,0 python eval_image_classifier.py --alsologtostderr --checkpoint_path=" + CHECKPOINT + " --dataset_dir=" + DATASET_DIR + " --dataset_name=imagenet --dataset_split_name=validation --model_name=inception_v4"
-            p = command_executor(cmd)
+
+            p = command_executor(data[i]["cmd"])
             os.chdir(pwd)
-
-        if (model_name in "MobileNet_v1"):
-            cmd = "OMP_NUM_THREADS=28 KMP_AFFINITY=granularity=fine,compact,1,0 python eval_image_classifier.py --alsologtostderr --checkpoint_path=" + CHECKPOINT + " --dataset_dir=" + DATASET_DIR + " --dataset_name=imagenet --dataset_split_name=validation --model_name=mobilenet_v1"
-            p = command_executor(cmd)
-            os.chdir(pwd)
-
-        if (model_name in "ResNet50_v1"):
-            cmd = "OMP_NUM_THREADS=28 KMP_AFFINITY=granularity=fine,compact,1,0 python eval_image_classifier.py --alsologtostderr --checkpoint_path=" + CHECKPOINT + " --dataset_dir=" + DATASET_DIR + " --dataset_name=imagenet --dataset_split_name=validation --model_name=resnet_v1_50 --labels_offset=1"
-            p = command_executor(cmd)
-            os.chdir(pwd)
-
-        #TODO:SSD-MobileNet_v1 does not run using ngraph, need to debug.
-        # if (model_name in "SSD-MobileNet_v1"):
-        #     command_executor(
-        #         "OMP_NUM_THREADS=28 KMP_AFFINITY=granularity=fine,compact,1,0 python object_detection/model_main.py --logtostderr --pipeline_config_path=object_detection/samples/configs/ssd_mobilenet_v1_coco.config --checkpoint_dir="
-        #         + CHECKPOINT + "--run_once=True")
-        #     os.chdir(pwd)
-
-        return model_name, p
-    except:
-        print(
-            "Please pass a valid Model name. Availble models are Inception_v4, MobileNet_v1, ResNet50_v1"
-        )
-        sys.exit(0)
+            return model_name, p
 
 
 def check_accuracy(model, p):
     #check if the accuracy of the model inference matches with the published numbers
-    accuracy = '[{"model_name" : "Inception_v4", "accuracy" : "0.95194"}, {"model_name" : "ResNet50_v1", "accuracy" : "0.752"}, {"model_name" : "MobileNet_v1", "accuracy" : "0.71018"}]'
+    #Accuracy values picked up from here https://github.com/tensorflow/models/tree/master/research/slim
+    accuracy = \
+    '[{"model_name" : "Inception_v4", "accuracy" : "0.802"},\
+     {"model_name" : "ResNet50_v1", "accuracy" : "0.752"}, \
+     {"model_name" : "MobileNet_v1", "accuracy" : "0.709"}]'
+
     data = json.loads(accuracy)
 
     for line in p.splitlines():
@@ -118,27 +120,42 @@ def check_accuracy(model, p):
             # Tolerance check
             diff = abs(float(top1_accuracy) - float(data[i]["accuracy"]))
             if (diff <= 0.001):
-                print("\nResult:Model Accuracy is expected for " + model + ' ' +
-                      top1_accuracy)
+                print("\nRESULT: Functional accuracy " + top1_accuracy +
+                      " is as expected for " + data[i]["model_name"])
             else:
-                print("\nResult:Model Accuracy is not as expected for " +
-                      model + ' ' + top1_accuracy)
+                print("\nRESULT: Functional accuracy " + top1_accuracy +
+                      " is not as expected for " + data[i]["model_name"] +
+                      "\nExpected accuracy is " + data[i]["accuracy"])
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Accuracy verification for TF models using ngraph.')
 
-    parser.add_argument('--model_name', help='Model name to run inference')
+    parser.add_argument(
+        '--model_name', help='Model name to run inference', required=True)
+    parser.add_argument(
+        '--models_dir',
+        help=
+        'Source of the model repository location on disk. If not specified, the local repository will be used. Example:ngraph-tf/diagnostics/model_accuracy/models'
+    )
     cwd = os.getcwd()
-    # This script must be run from this location
-    assert '/'.join(
-        cwd.split('/')[-3:]) == 'ngraph-tf/diagnostics/model_accuracy'
 
+    repo = "https://github.com/tensorflow/models.git"
+    download_repo(repo)
     args = parser.parse_args()
 
+    if (args.models_dir):
+        models_dir = args.models_dir
+    else:
+        models_dir = None
     #Just takes in one model at a time for now
     #TODO(Sindhu): Run multiple or ALL models at once and compare accuracy.
 
-    model_name, p = run_inference(args.model_name)
-    check_accuracy(model_name, p)
+    try:
+        model_name, p = run_inference(args.model_name, models_dir)
+        check_accuracy(model_name, p)
+    except:
+        print(
+            "Please pass a valid Model name. Availble models are Inception_v4, MobileNet_v1, ResNet50_v1"
+        )
