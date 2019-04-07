c61eb162ce7ceab0efedefb366a03aa64d666b8e
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-04-06 19:03:30 -0700

    Taking care of backend setting

--- src/ngraph_mark_for_clustering.cc
+++ src/ngraph_mark_for_clustering.cc
@@ -266,10 +266,6 @@ Status MarkForClustering(Graph* graph,
         TF_RETURN_IF_ERROR(GetNodeAttr(n->attrs(), "is_training", result));
         return Status::OK();
       };
-      confirmation_function_map["GatherV2"] = [](Node* n, bool* result) {
-        *result = (BackendManager::GetCurrentlySetBackendName() == "NNPI");
-        return Status::OK();
-      };
       confirmation_function_map["_FusedConv2D"] = SimpleConfirmationFunction();
       confirmation_function_map["Greater"] = SimpleConfirmationFunction();
       confirmation_function_map["GreaterEqual"] = SimpleConfirmationFunction();
@@ -616,6 +612,34 @@ Status MarkForClustering(Graph* graph,
     }
   }
 
+  // Set Attributes for nodes marked for clustering
+  // 1. Set Attribute "_ngraph_marked_for_clustering" as "true"
+  // 2. Set the backend for each op
+  // 3. Set any other attributes as defined in set_attribute_map
+  string current_backend = BackendManager::GetCurrentlySetBackendName();
+  const char* ng_backend_env_value = std::getenv("NGRAPH_TF_BACKEND");
+  if (ng_backend_env_value != nullptr) {
+    string backend_env = std::string(ng_backend_env_value);
+    if (backend_env.empty() ||
+        !BackendManager::IsSupportedBackend(backend_env)) {
+      return errors::Internal("NGRAPH_TF_BACKEND: ", backend_env,
+                              " is not supported");
+    }
+    current_backend = backend_env;
+    // TODO: set backend. Then don't use current_backend
+  }
+  cout << "Found NG Backend " << current_backend << "\n";
+
+  // Right now it cannot be inside the if(!initialized) block, because it is
+  // backend dependent, which might change with different sess.run()s
+  confirmation_function_map["GatherV2"] = [&current_backend](Node* n,
+                                                             bool* result) {
+    // TODO: replace current_backend ->
+    // BackendManager::GetCurrentlySetBackendName()
+    *result = (current_backend == "NNPI");
+    return Status::OK();
+  };
+
   std::unordered_map<string, int> no_support_histogram;
   std::unordered_map<string, int> fail_confirmation_histogram;
   std::unordered_map<string, int> fail_constraint_histogram;
@@ -700,23 +724,7 @@ Status MarkForClustering(Graph* graph,
     std::cout << "\n";
   }
 
-  // Set Attributes for nodes marked for clustering
-  // 1. Set Attribute "_ngraph_marked_for_clustering" as "true"
-  // 2. Set the backend for each op
-  // 3. Set any other attributes as defined in set_attribute_map
-  string current_backend = BackendManager::GetCurrentlySetBackendName();
-  const char* ng_backend_env_value = std::getenv("NGRAPH_TF_BACKEND");
-  if (ng_backend_env_value != nullptr) {
-    string backend_env = std::string(ng_backend_env_value);
-    if (backend_env.empty() ||
-        !BackendManager::IsSupportedBackend(backend_env)) {
-      return errors::Internal("NGRAPH_TF_BACKEND: ", backend_env,
-                              " is not supported");
-    }
-    current_backend = backend_env;
-  }
-  NGRAPH_VLOG(5) << "Found NG Backend " << current_backend;
-
+  std::cout << "current_backend:: " << current_backend << "\n";
   for (auto node : nodes_marked_for_clustering) {
     // TODO(amprocte): move attr name to a constant
     node->AddAttr("_ngraph_marked_for_clustering", true);
