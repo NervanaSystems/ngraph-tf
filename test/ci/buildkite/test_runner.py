4acf39d1b9673dc35e630830aebd6df1faa5f571
avijit-nervana <avijit.chakraborty@intel.com>
2019-04-07 20:11:30 -0700

    Fixed the TensorFlow Python tests

--- test/ci/buildkite/test_runner.py
+++ test/ci/buildkite/test_runner.py
@@ -106,8 +106,8 @@ def main():
     elif (arguments.test_bazel_build):
         run_bazel_build()
     elif (arguments.test_tf_python):
-        run_tensorflow_pytests_from_artifacts('./', 'build_cmake/tensorflow',
-                                              False)
+        run_tensorflow_pytests_from_artifacts(
+            './', arguments.artifacts_dir + '/tensorflow/python', False)
     elif (arguments.test_resnet):
         batch_size = 128
         iterations = 10
